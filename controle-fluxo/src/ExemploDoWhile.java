568098279503ad7dcf91cf02da99d81a3a474200
Pamelakarina <pam.temah@gmail.com>
2024-05-25 17:24:53 -0300

    Exemplo de Do While

---
+++ controle-fluxo/src/ExemploDoWhile.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+public class ExemploDoWhile {
+    public static void main(String[] args) {
+        System.out.println("Discando... ");
+        do { 
+        //executando repetidas vezes até alguém atender. 
+        System.out.println("Telefone tocando... ");
+
+        } while(tocando());
+
+        System.out.println("Alo?! ");
+
+    }
+    private static boolean tocando() {
+        boolean atendeu = new Random().nextInt(3) == 1;
+        System.out.println("Atendeu? " + atendeu);
+        //negando o ato de contnuar tocando
+        return ! atendeu;
+    }
+}
